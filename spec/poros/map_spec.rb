a4ef47ec31026920264e101eec05bf3f804e3d17
Sage Skaff <skaffsage@gmail.com>
2023-01-08 14:59:48 -0800

    Add poro tests

---
+++ spec/poros/map_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'map' do
+    it 'exists' do
+        map = MapFacade.directions('5224 W 25th Ave, Denver, CO 80214', '1985 Sheridan Blvd, Edgewater CO 80214')
+        expected_instructions = ["Head east on W 25th Ave. Go for 246 ft.",
+                            "Turn right onto Sheridan Blvd (CO-95).",
+                            "Arrive at Sheridan Blvd (CO-95). Your destination is on the right."]
+
+        expect(map).to be_a(Map)
+        expect(map.distance).to eq(0.4176)
+        expect(map.travel_time).to eq('00:01:03')
+        expect(map.instructions).to eq(expected_instructions)
+    end
+end
--- spec/poros/station_spec.rb
+++ spec/poros/station_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
-describe 'station facade' do
-    it 'creates station objects from station service data' do
+describe 'station' do
+    it 'exists' do
         station = StationFacade.nearest_station('5224 W 25th Ave, Denver, CO 80214')
+
         expect(station).to be_a(Station)
+        expect(station.full_address).to eq('1985 Sheridan Blvd, Edgewater CO 80214')
     end
 end
